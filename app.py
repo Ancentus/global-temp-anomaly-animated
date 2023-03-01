72c65cdada1169c0aa2fe5cf00144ddaa2a9861d
Ancentus <mancentus7@gmail.com>
2023-03-01 04:53:30 +0300

    Update title; Hide period label

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ df = pd.read_csv('annual_csv.csv')
 df_gistemp = df[df.Source == 'GISTEMP'][['Year', 'Mean']].set_index('Year')
 
 # Create an animated line chart using plot_animated
-df_gistemp.plot_animated(kind='line', filename="temp-anomaly-over-time-line.mp4",period_fmt="%Y", title='Temperature Anomaly (1880 - 2016)', enable_progress_bar=True)
+df_gistemp.plot_animated(kind='line', filename="temp-anomaly-over-time-line.mp4",period_fmt="%Y", title='Global Temperature Anomaly (1880 - 2016)', enable_progress_bar=True, period_label=False)
 
